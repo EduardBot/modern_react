659d2f7f6406c91471ef8cd2612154192db6f889
Eduard <edjan300@gmail.com>
2023-04-14 14:57:24 +0300

    Стили кнопки модулями и передача всех пропсов

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import "./components/styles/style.css";
 // import Counter from "./components/Counter";
 // import Value from "./components/Value";
 import PostList from "./components/PostList";
+import Button from "./components/UI/button/Button";
 function App() {
     const [posts, setPosts] = useState([
       {id: 1, title: 'карнавал', body: "description"},
       {id: 2, title: 'карнавал 2', body: "description"},
       {id: 3, title: 'карнавал 3', body: "description"},
     ])
-    const [posts2, setPosts2] = useState([
-      {id: 1, title: 'маскарад', body: "description"},
-      {id: 2, title: 'маскарад 2', body: "description"},
-      {id: 3, title: 'маскарад 3', body: "description"},
-    ])
+    // const [posts2, setPosts2] = useState([
+    //   {id: 1, title: 'маскарад', body: "description"},
+    //   {id: 2, title: 'маскарад 2', body: "description"},
+    //   {id: 3, title: 'маскарад 3', body: "description"},
+    // ])
 
   return (
     <div className="App">
       {/* <Counter />
       <Value /> */}
+      {/* <PostList posts={posts2} title={"Список постов 2"}/> */}
+
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <Button disabled>Создать пост</Button>
+      </form>
       <PostList posts={posts} title={"Список постов 1"}/>
-      <PostList posts={posts2} title={"Список постов 2"}/>
     </div>
   );
 }
